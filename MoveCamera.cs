33a47cbe36516f980fa7e1543ff67734f5616552
LuisDevy <99799321+luisdevy@users.noreply.github.com>
2022-04-04 19:34:10 +0200

    Add files via upload

---
+++ MoveCamera.cs
@@ -0,0 +1,11 @@
+//Basic MoveCamera Script...*** by  LUIS
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    public Transform player;
+
+    void Update() {
+        transform.position = player.transform.position;
+    }
+}
